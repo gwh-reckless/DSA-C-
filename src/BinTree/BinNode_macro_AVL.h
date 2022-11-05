f7c0fca03d460dcf7b189eba42c79fa3ea508483
gwh-reckless <williamguogo@gmail.com>
2022-11-06 05:58:50 +0800

    Add: BinNode macro AVL

---
+++ src/BinTree/BinNode_macro_AVL.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "_share/util.h"
+
+#define HeightUpated(x) \
+  ( (x).height = 1 + max( stature((x).lc) , stature((x)->rc)) )
+
+#define Balanced(x) (stature((x).lc) == stature((x).rc)) // The ideal balance condition
+#define BalFac(x) (stature((x).lc) - stature((x).rc)) // Balance Factor
+#define AvlBalanced(x) ( (-2 < BalFac(x)) && (BalFac(x) < 2)) // AVL Blanced condition
+
+
